6fa42038af226af8610362c26fc32e2115f0e95f
timmorey <timmorey@gmail.com>
2013-02-26 07:21:33 -0500

    adding the beginnings of a hdf5 test

---
+++ h5write/h5write.c
@@ -0,0 +1,346 @@
+/**
+   h5write.c - Created by Timothy Morey on 2/22/2013
+
+   This file attempts to replicate PISM's write pattern to test the performance
+   of the hdf5 output method.
+ */
+
+#include <math.h>
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <hdf5.h>
+
+#define NVARS 4
+#define NDIMS 4
+#define XNAME "x"
+#define XDIM 1
+#define XSIZE 1501
+#define YNAME "y"
+#define YDIM 2
+#define YSIZE 2801
+#define ZNAME "z"
+#define ZDIM 3
+#define ZSIZE 401
+#define TNAME "t"
+#define TDIM 0
+
+
+int GetLocalBounds(int gwidth, int gheight, int rank, int size, 
+		   int* x, int* y, int* width, int* height)
+{
+  int retval = 0;
+  int sizex = 0;
+  int sizey = 0;
+
+  // Using PISM's algorithm for distributing gwidth columns and gheight rows of
+  // grid cells over size processors:
+  sizex = (int)(0.5 + sqrt(((double)gwidth)*((double)size)/((double)gheight)));
+  if(sizex == 0)
+    sizex = 1;
+
+  while(sizex > 0)
+  {
+    sizey = size / sizex;
+    if(size == sizex*sizey)
+      break;
+    sizex--;
+  }
+
+  if(gwidth > gheight && sizex < sizey)
+  {
+    int temp = sizex;
+    sizex = sizey;
+    sizey = temp;
+  }
+
+  if(sizex == 0 || 
+     sizey == 0 ||
+     (gwidth / sizex) < 1 ||
+     (gheight / sizey) < 1)
+  {
+    printf("Failed to distribute %d columns and %d rows over %d processors\n", 
+	   gwidth, gheight, size);
+    retval = 1;
+  }
+  else
+  {
+    int extrac = gwidth % sizex;
+    int extrar = gheight % sizey;
+    int r = rank / sizex;
+    int c = rank % sizex;
+
+    *width = gwidth / sizex;
+    if(c < extrac)
+      *width += 1;
+
+    *height = gheight / sizey;
+    if(r < extrar)
+      *height += 1;
+
+    *x = (*width) * c + (c < extrac ? c : extrac);
+    *y = (*height) * r + (r < extrar ? r : extrar);
+  }
+  
+  return retval;
+}
+
+int main(int argc, char* argv[])
+{
+  char c;
+  int ix, iy, iz, i;
+  MPI_Comm mpicomm;
+  MPI_Info mpiinfo;
+  int mpirank;
+  int mpisize;
+  double *data3d, *data2d, *x, *y, *z, t;
+  int localx, localy, localwidth, localheight;
+  const char* filename = "output.h5";
+  hid_t fileid, plistid, dsid;
+  hsize_t dimsize[NDIMS], maxdimsize[NDIMS], chunksize[NDIMS];
+
+/*
+  int ncresult;
+  int ncid, tvarid, xvarid, yvarid, zvarid;
+  int dimid[NDIMS], var3did[NVARS], var2did[NVARS];
+  size_t start[NDIMS], count[NDIMS];  
+  const char* filename = "output.nc";
+  char varname[32];
+*/
+
+  mpicomm = MPI_COMM_WORLD;
+  mpiinfo = MPI_INFO_NULL;
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(mpicomm, &mpisize);
+  MPI_Comm_rank(mpicomm, &mpirank);
+
+  if(! mpirank) printf("Creating some data...\n");
+  
+  // Distribute our data values in a pism-y way
+  GetLocalBounds(XSIZE, YSIZE, mpirank, mpisize,
+                 &localx, &localy, &localwidth, &localheight);
+  printf("Rank%02d: x=%d, y=%d, width=%d, height=%d\n",
+         mpirank, localx, localy, localwidth, localheight);
+  
+  data2d = (double*)malloc(localwidth * localheight * sizeof(double));
+  data3d = (double*)malloc(localwidth * localheight * ZSIZE * sizeof(double));
+  x = (double*)malloc(localwidth * sizeof(double));
+  y = (double*)malloc(localheight * sizeof(double));
+  z = (double*)malloc(ZSIZE * sizeof(double));
+  t = 0.0;
+  for(ix = 0; ix < localwidth; ix++)
+  {
+    x[ix] = ix + localx;
+    for(iy = 0; iy < localheight; iy++)
+    {
+      y[iy] = iy + localy;
+      data2d[ix*localheight + iy] = (ix+localx)*localheight + iy+localy;
+      for(iz = 0; iz < ZSIZE; iz++)
+      {
+        z[iz] = iz;
+        data3d[ix*localheight*ZSIZE + iy*ZSIZE + iz] = 
+          (ix+localx)*YSIZE*ZSIZE + (iy+localy)*ZSIZE + iz;
+      }
+    }
+  }
+
+
+  if(! mpirank) printf("Creating HDF5 file...\n");
+
+  plistid = H5Pcreate(H5P_FILE_ACCESS);
+  H5Pset_fapl_mpio(plistid, mpicomm, mpiinfo);
+
+  // TODO: this seems like a good place to put optimizations, and indeed
+  // PISM is adding several additional properties, like setting block sizes,
+  // cache eviction policies, fs striping parameters, etc.
+
+  fileid = H5Fcreate(filename, H5F_ACC_TRUNC, H5P_DEFAULT, plistid);
+  H5Pclose(plistid);
+
+  if(! mpirank) printf("Setting up dimensions...\n");
+
+  dimsize[0] = 0;
+  maxdimsize[0] = H5S_UNLIMITED;
+  chunksize[0] = 1;
+
+  dsid = H5Screate_simple(1, dimsize, maxdimsize);
+  plistid = H5Pcreate(H5P_DATASET_CREATE);
+  dimid = H5Dcreate(fileid, TNAME, H5T_NATIVE_DOUBLE, dsid, 
+                    H5P_DEFAULT, plistid, H5P_DEFAULT);
+  H5Pclose(plistid);
+  H5DSset_scale(dimid, TNAME);
+  H5Sclose(dsid);
+  H5Dclose(dimid);
+
+
+/*
+  if(! mpirank) printf("Writing t...\n");
+
+  nc_enddef(ncid);
+
+  ncresult = nc_var_par_access(ncid, tvarid, NC_COLLECTIVE);
+  ncerror("nc_var_par_access", ncresult);
+
+  start[0] = 0;  count[0] = 1;
+  ncresult = nc_put_vara_double(ncid, tvarid, start, count, &t);
+  ncerror("nc_put_var (t)", ncresult);
+
+  nc_redef(ncid);
+
+  ncresult = nc_def_dim(ncid, XNAME, XSIZE, &dimid[XDIM]);  
+  ncerror("nc_def_dim", ncresult);
+
+  ncresult = nc_def_var(ncid, XNAME, NC_DOUBLE, 1, &dimid[XDIM], &xvarid);
+  ncerror("nc_def_var", ncresult);
+
+  if(! mpirank) printf("Writing x...\n");
+
+  nc_enddef(ncid);
+
+  ncresult = nc_var_par_access(ncid, xvarid, NC_COLLECTIVE);
+  ncerror("nc_var_par_access", ncresult);
+
+  start[0] = localx;  count[0] = localwidth;
+  ncresult = nc_put_vara_double(ncid, xvarid, start, count, x);
+  ncerror("nc_put_vara_all (x)", ncresult);
+
+  nc_redef(ncid);
+
+  ncresult = nc_def_dim(ncid, YNAME, YSIZE, &dimid[YDIM]);  
+  ncerror("nc_def_dim", ncresult);
+
+  ncresult = nc_def_var(ncid, YNAME, NC_DOUBLE, 1, &dimid[YDIM], &yvarid);
+  ncerror("nc_def_var", ncresult);
+
+  if(! mpirank) printf("Writing y...\n");
+
+  nc_enddef(ncid);
+
+  ncresult = nc_var_par_access(ncid, yvarid, NC_COLLECTIVE);
+  ncerror("nc_var_par_access", ncresult);
+
+  start[0] = localy;  count[0] = localheight;
+  ncresult = nc_put_vara_double(ncid, yvarid, start, count, y);
+  ncerror("nc_put_vara_all (y)", ncresult);
+
+  nc_redef(ncid);
+
+  ncresult = nc_def_dim(ncid, ZNAME, ZSIZE, &dimid[ZDIM]);  
+  ncerror("nc_def_dim", ncresult);
+
+  ncresult = nc_def_var(ncid, ZNAME, NC_DOUBLE, 1, &dimid[ZDIM], &zvarid);
+  ncerror("nc_def_var", ncresult);
+
+  if(! mpirank) printf("Writing z...\n");
+
+  nc_enddef(ncid);
+
+  ncresult = nc_var_par_access(ncid, zvarid, NC_COLLECTIVE);
+  ncerror("nc_var_par_access", ncresult);
+
+  ncresult = nc_put_var_double(ncid, zvarid, z);
+  ncerror("nc_put_var (z)", ncresult);
+
+  nc_redef(ncid);
+
+  if(! mpirank) printf("Defining variables...\n");
+  
+  for(i = 0; i < NVARS; i++)
+  {
+    sprintf(varname, "var3d%02d", i);
+    ncresult = nc_def_var(ncid, varname, NC_DOUBLE, NDIMS, dimid, &var3did[i]);
+    ncerror("nc_def_var", ncresult);
+
+    sprintf(varname, "var2d%02d", i);
+    ncresult = nc_def_var(ncid, varname, NC_DOUBLE, NDIMS-1, dimid, &var2did[i]);
+  }
+
+  if(! mpirank) printf("Closing...\n");
+
+  ncresult = nc_close(ncid);
+  ncerror("nc_close", ncresult);
+
+  for(i = 0; i < NVARS; i++)
+  {
+    if(! mpirank) printf("Openning...\n");
+
+    ncresult = nc_open_par(filename, NC_MPIIO | NC_WRITE, 
+                           mpicomm, mpiinfo, &ncid);
+    ncerror("nc_open_par", ncresult);
+
+    nc_enddef(ncid);
+    
+    if(! mpirank) printf("Writing data...\n");
+
+    start[XDIM] = localx; 
+    start[YDIM] = localy;  
+    start[ZDIM] = 0;  
+    start[TDIM] = 0;
+    
+    count[XDIM] = localwidth;  
+    count[YDIM] = localheight;  
+    count[ZDIM] = ZSIZE;
+    count[TDIM] = 1;
+
+    ncresult = nc_var_par_access(ncid, var3did[i], NC_COLLECTIVE);
+    ncerror("nc_var_par_access", ncresult);
+
+    ncresult = nc_put_vara_double(ncid, var3did[i], start, count, data3d);
+    ncerror("nc_put_vara_double", ncresult);
+    
+    if(! mpirank) printf("Closing file...\n");
+    
+    ncresult = nc_close(ncid);
+    ncerror("nc_close", ncresult);
+  }
+
+  for(i = 0; i < NVARS; i++)
+  {
+    if(! mpirank) printf("Openning...\n");
+
+    ncresult = nc_open_par(filename, NC_MPIIO | NC_WRITE, 
+                           mpicomm, mpiinfo, &ncid);
+    ncerror("nc_open_par", ncresult);
+
+    nc_enddef(ncid);
+    
+    if(! mpirank) printf("Writing data...\n");
+
+    start[XDIM] = localx; 
+    start[YDIM] = localy;  
+    start[TDIM] = 0;
+    
+    count[XDIM] = localwidth;  
+    count[YDIM] = localheight;  
+    count[TDIM] = 1;
+
+    ncresult = nc_var_par_access(ncid, var2did[i], NC_COLLECTIVE);
+    ncerror("nc_var_par_access", ncresult);
+
+    ncresult = nc_put_vara_double(ncid, var2did[i], start, count, data2d);
+    ncerror("nc_put_vara_double", ncresult);
+    
+    if(! mpirank) printf("Closing file...\n");
+    
+    ncresult = nc_close(ncid);
+    ncerror("nc_close", ncresult);
+  }
+
+  if(! mpirank) printf("Done.\n");
+*/
+
+  H5Fclose(fileid);
+
+  free(data2d);
+  free(data3d);
+  free(x);
+  free(y);
+  free(z);
+
+  MPI_Finalize();
+
+  return 0;
+}
